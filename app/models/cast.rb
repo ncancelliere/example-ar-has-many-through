cc2bf92e92d5bf7e3a035001bc817faaf52f9bbb
Nicholas Cancelliere <ncancelliere@gmail.com>
2013-05-23 22:04:25 -0500

    add validation to disallow dupe cast entries for an actor in a given movie

--- app/models/cast.rb
+++ app/models/cast.rb
@@ -2,6 +2,7 @@ class Cast < ActiveRecord::Base
   attr_accessible :actor, :character, :movie
 
   validates :character, :presence => true
+  validates :actor_id, :uniqueness => { :scope => :movie_id, :message => 'already stars in this movie' }
 
   belongs_to :movie
   belongs_to :actor
